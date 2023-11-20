41a6c1088fbb20a1a23fdeaa5d85ba954b09b0a7
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-11-20 10:21:08 +0530

    Success fully data is pushed to DB

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
+import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -13,20 +18,17 @@ interface ModalProps {
   secondaryActionLabel?: string;
 }
 
-import { IoMdClose } from 'react-icons/io';
-import Button from "../Button";
-
-const Modal: React.FC<ModalProps> = ({
-  isOpen,
-  onClose,
-  onSubmit,
-  title,
-  body,
-  footer,
-  actionLabel,
-  secondaryAction,
-  secondaryActionLabel,
+const Modal: React.FC<ModalProps> = ({ 
+  isOpen, 
+  onClose, 
+  onSubmit, 
+  title, 
+  body, 
+  actionLabel, 
+  footer, 
   disabled,
+  secondaryAction,
+  secondaryActionLabel
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
 
@@ -35,19 +37,23 @@ const Modal: React.FC<ModalProps> = ({
   }, [isOpen]);
 
   const handleClose = useCallback(() => {
-    if (disabled) return;
-
+    if (disabled) {
+      return;
+    }
+  
     setShowModal(false);
     setTimeout(() => {
       onClose();
-    }, 300);
-  }, [disabled, onClose]);
+    }, 300)
+  }, [onClose, disabled]);
 
   const handleSubmit = useCallback(() => {
-    if (disabled) return;
+    if (disabled) {
+      return;
+    }
 
     onSubmit();
-  }, [disabled, onSubmit]);
+  }, [onSubmit, disabled]);
 
   const handleSecondaryAction = useCallback(() => {
     if (disabled || !secondaryAction) {
@@ -55,125 +61,121 @@ const Modal: React.FC<ModalProps> = ({
     }
 
     secondaryAction();
-  }, [disabled, secondaryAction]);
+  }, [secondaryAction, disabled]);
 
   if (!isOpen) {
     return null;
   }
 
   return (
-    <main>
+    <>
       <div
         className="
-          flex
-          justify-center
-          items-center
-          fixed
-          inset-0
-          z-50
-          outline-none
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
           focus:outline-none
           bg-neutral-800/70
         "
       >
-        <div
-          className="
-          relative w-full max-w-[30rem] h-auto overflow-y-scroll scrollbar-hide
+        <div className="
+          relative 
+          w-full
+          md:w-4/6
+          lg:w-3/6
+          xl:w-2/5
+          my-6
+          mx-auto 
+          h-full 
+          lg:h-auto
+          md:h-auto
           "
         >
-          {/* CONTENT */}
-          <div
-            className={`
+          {/*content*/}
+          <div className={`
+            translate
+            duration-300
+            h-full
+            ${showModal ? 'translate-y-0' : 'translate-y-full'}
+            ${showModal ? 'opacity-100' : 'opacity-0'}
+          `}>
+            <div className="
               translate
-              duration-300
-              ${showModal ? 'translate-y-0' : 'translate-y-full'}
-              ${showModal ? 'opacity-100' : 'opacity-0'}
-            `}
-          >
-            <div
-              className="
-                translate
-                border-0
-                rounded-lg
-                shadow-lg
-                flex
-                flex-col
-                w-full
-                bg-white
-                outline-none
-                focus:outline-none
-                sticky
-              "
+              h-full
+              lg:h-auto
+              md:h-auto
+              border-0 
+              rounded-lg 
+              shadow-lg 
+              relative 
+              flex 
+              flex-col 
+              w-full 
+              bg-white 
+              outline-none 
+              focus:outline-none
+            "
             >
-              {/* Header */}
-              <div
-                className="
-                  flex
-                  items-center
-                  justify-center
-                  rounded-t
-                  p-6
-                  sticky
-                  z-[60]
-                  bg-white
-                  top-0
-                  border-b-[1px]
+              {/*header*/}
+              <div className="
+                flex 
+                items-center 
+                p-6
+                rounded-t
+                justify-center
+                relative
+                border-b-[1px]
                 "
               >
-                {/* close modal */}
                 <button
-                  onClick={handleClose}
                   className="
-                    p-2
-                    border-0
-                    hover:bg-neutral-100
-                    rounded-full
+                    p-1
+                    border-0 
+                    hover:opacity-70
                     transition
                     absolute
                     left-9
                   "
+                  onClick={handleClose}
                 >
                   <IoMdClose size={18} />
                 </button>
-
-                {/* modal title */}
-                <h3 className="text-lg font-semibold">{title}</h3>
+                <div className="text-lg font-semibold">
+                  {title}
+                </div>
               </div>
-
-              {/* Body */}
-              <div
-                className="
-                  md:overflow-y-auto
-                  md:scrollbar-hide
-                  sm:h-[80vh] md:h-[70vh] lg:h-[60vh]
-                "
-              >
-                <div className="px-6 pt-4 flex-auto">{body}</div>
+              {/*body*/}
+              <div className="relative p-6 flex-auto">
+                {body}
               </div>
-
-              {/* Footer */}
+              {/*footer*/}
               <div className="flex flex-col gap-2 p-6">
-                <div
+                <div 
                   className="
-                    flex
-                    flex-row
-                    items-center
-                    gap-4
+                    flex 
+                    flex-row 
+                    items-center 
+                    gap-4 
                     w-full
                   "
                 >
                   {secondaryAction && secondaryActionLabel && (
-                    <Button
-                      outline
-                      disabled={disabled}
-                      label={secondaryActionLabel}
+                    <Button 
+                      disabled={disabled} 
+                      label={secondaryActionLabel} 
                       onClick={handleSecondaryAction}
-                    />
+                      outline
+                    />  
                   )}
-
-                  <Button
-                    disabled={disabled}
-                    label={actionLabel}
+                  <Button 
+                    disabled={disabled} 
+                    label={actionLabel} 
                     onClick={handleSubmit}
                   />
                 </div>
@@ -183,8 +185,8 @@ const Modal: React.FC<ModalProps> = ({
           </div>
         </div>
       </div>
-    </main>
+    </>
   );
-};
+}
 
 export default Modal;
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,17 +1,28 @@
 "use client";
 
+import toast from "react-hot-toast";
+import axios from "axios";
+import dynamic from "next/dynamic";
+import Input from "../inputs/Input";
+import ImageUpload from "../inputs/ImageUpload";
 import { useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+
 import Modal from "./Modal";
+import Heading from "../Heading";
 
 import useRentModal from "@/app/hooks/useRentModal";
-import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm
+} from "react-hook-form";
+
 import CountrySelect from "../inputs/CountrySelect";
-import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
-import ImageUpload from "../inputs/ImageUpload";
 
 
 enum STEPS {
@@ -24,6 +35,7 @@ enum STEPS {
 }
 
 const RentModal = () => {
+  const router = useRouter();
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
@@ -31,9 +43,11 @@ const RentModal = () => {
   const {
     register,
     handleSubmit,
+    reset,
     setValue,
     watch,
     formState: {
+      isLoading,
       errors
     }
   } = useForm<FieldValues>({
@@ -60,6 +74,7 @@ const RentModal = () => {
   const Map = useMemo(() => dynamic(() => import('../Map'), {ssr: false}), [location])
 
   const setCustomValue = (id: string, value: any) => {
+
     setValue(id, value, {
       shouldDirty: true,
       shouldValidate: true,
@@ -75,6 +90,24 @@ const RentModal = () => {
     setStep((value) => value + 1);
   }
 
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    axios.post('/api/listings', data)
+      .then(() => {
+        toast.success('Listing created');
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose();
+      })
+      .catch (() => {
+        toast.error('Something went wrong')
+      })
+    
+  }
   const actionLabel = useMemo(() => {
     if (step === STEPS.PRICE) {
       return 'Create';
@@ -191,11 +224,64 @@ const RentModal = () => {
     )
   }
   
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subTitle="Short and sweet works best"
+        />
+
+        <Input 
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+
+        <hr />
+
+        <Input 
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subTitle="How much do you charge per night"
+        />
+
+        <Input
+          id="price"
+          label="Price"
+          formatPrice 
+          type="number"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
   return ( 
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
